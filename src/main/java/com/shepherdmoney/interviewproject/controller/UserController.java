1215716c90de73de4e0efe124f0e8e5014fa9daa
thuyluu <thuyluu9595@gmail.com>
2023-08-07 23:20:42 -0700

    Completed UserController.java

--- src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
@@ -1,19 +1,31 @@
 package com.shepherdmoney.interviewproject.controller;
 
+import com.shepherdmoney.interviewproject.model.User;
+import com.shepherdmoney.interviewproject.repository.UserRepository;
 import com.shepherdmoney.interviewproject.vo.request.CreateUserPayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
     // TODO: wire in the user repository (~ 1 line)
+    @Autowired
+    private UserRepository userRepository;
 
     @PutMapping("/user")
     public ResponseEntity<Integer> createUser(@RequestBody CreateUserPayload payload) {
         // TODO: Create an user entity with information given in the payload, store it in the database
         //       and return the id of the user in 200 OK response
-        return null;
+        User newUser = new User();
+        newUser.setName(payload.getName());
+        newUser.setEmail(payload.getEmail());
+        userRepository.save(newUser);
+        return new ResponseEntity<>(newUser.getId(),HttpStatus.OK);
     }
 
     @DeleteMapping("/user")
@@ -21,6 +33,13 @@ public class UserController {
         // TODO: Return 200 OK if a user with the given ID exists, and the deletion is successful
         //       Return 400 Bad Request if a user with the ID does not exist
         //       The response body could be anything you consider appropriate
-        return null;
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()){
+            userRepository.delete(user.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 }
